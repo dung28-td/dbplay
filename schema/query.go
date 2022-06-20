566155705c7bfd0ffeaeef52ca8306cec2042dde
Dung Tran <trnidng2810@gmail.com>
2022-06-20 22:48:17 +0700

    List all connections

--- schema/query.go
+++ schema/query.go
@@ -1,15 +1,29 @@
 package schema
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/dung28-td/dbplay/db"
+	"github.com/dung28-td/dbplay/db/models"
+	"github.com/graphql-go/graphql"
+)
+
+var connections = graphql.Field{
+	Type: graphql.NewList(connectionType),
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		var connections []models.Connection
+
+		if err := db.DB.NewSelect().
+			Model(&connections).
+			Scan(p.Context); err != nil {
+			return nil, err
+		}
+
+		return connections, nil
+	},
+}
 
 var query = graphql.NewObject(graphql.ObjectConfig{
 	Name: "RootQuery",
 	Fields: graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
+		"connections": &connections,
 	},
 })
--- schema/schema.go
+++ schema/schema.go
@@ -2,6 +2,22 @@ package schema
 
 import "github.com/graphql-go/graphql"
 
+var connectionType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Connection",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.ID,
+		},
+		"name": &graphql.Field{
+			Type: graphql.String,
+		},
+		"dsn": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	},
+})
+
 var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
-	Query: query,
+	Query:    query,
+	Mutation: mutation,
 })
